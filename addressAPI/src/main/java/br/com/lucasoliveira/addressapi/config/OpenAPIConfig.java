d10c3beab8c9810bc0a40b1e8e1ae534effa71b0
Lucas Oliveira <lukecoliver@yahoo.com>
2024-11-24 20:00:14 -0300

    Config wiremock e docker

--- addressAPI/src/main/java/br/com/lucasoliveira/addressapi/config/OpenAPIConfig.java
+++ addressAPI/src/main/java/br/com/lucasoliveira/addressapi/config/OpenAPIConfig.java
@@ -2,6 +2,7 @@ package br.com.lucasoliveira.addressapi.config;
 
 import java.util.List;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,31 +16,22 @@ import io.swagger.v3.oas.models.servers.Server;
 @Configuration
 public class OpenAPIConfig {
 
-    @Value("${addressAPI.openapi.dev-url}")
-    private String url;
-
-
-
     @Bean
-    public OpenAPI myOpenAPI() {
-        Server server = new Server();
-        server.setUrl(url);
-        server.setDescription("Server URL in Development environment");
-
-
-        Contact contact = new Contact();
-        contact.setEmail("lucascoliveira.geti@gmail.com");
-        contact.setName("Lucas Oliveira");
-
-        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
-
-        Info info = new Info()
-                .title("Tutorial Management API")
-                .version("1.0")
-                .contact(contact)
-                .description("This API exposes endpoints to manage tutorials.").termsOfService("https://www.lucasoliveira.com/terms")
-                .license(mitLicense);
-
-        return new OpenAPI().info(info).servers(List.of(server));
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("AddressAPI Documentation")
+                        .description("API para consultar informações de CEPs.")
+                        .version("v1")
+                        .contact(new Contact()
+                                .name("Lucas Oliveira")
+                                .email("lucas.oliveira@example.com")
+                                .url("https://github.com/LukeColiver"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Projeto no GitHub")
+                        .url("https://github.com/LukeColiver/desafio-f1rst-api"));
     }
 }
--- addressAPI/src/main/java/br/com/lucasoliveira/addressapi/controller/LogController.java
+++ addressAPI/src/main/java/br/com/lucasoliveira/addressapi/controller/LogController.java
@@ -2,6 +2,9 @@ package br.com.lucasoliveira.addressapi.controller;
 
 import br.com.lucasoliveira.addressapi.entity.LogApi;
 import br.com.lucasoliveira.addressapi.service.LogService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,14 @@ public class LogController {
 
     private final LogService logService;
 
+    @Operation(
+            summary = "Obtém todos os logs da API",
+            description = "Retorna uma lista de todos os registros de log armazenados no sistema."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lista de logs retornada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro ao obter os logs")
+    })
     @GetMapping
     public ResponseEntity<List<LogApi>> getAllLogs() {
         List<LogApi> logs = logService.getAllLogs();
@@ -22,6 +33,15 @@ public class LogController {
     }
 
 
+    @Operation(
+            summary = "Filtra logs pelo status de resposta",
+            description = "Retorna uma lista de logs filtrados com base no status de resposta especificado."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Lista de logs filtrada retornada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Parâmetro de status de resposta inválido"),
+            @ApiResponse(responseCode = "500", description = "Erro ao filtrar os logs")
+    })
     @GetMapping("/by-status")
     public ResponseEntity<List<LogApi>> getLogsByResponseStatus(@RequestParam String responseStatus) {
         List<LogApi> logs = logService.getLogsByResponseStatus(responseStatus);
